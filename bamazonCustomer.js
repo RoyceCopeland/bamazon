4a31de6888faf8609efa7ed0733c1fd521838c01
Royce Copeland <roycecopeland@Royces-iMac.local>
2017-08-14 21:36:55 -0400

    working checkpoint

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,11 +7,55 @@ var connection = mySql.createConnection({
     user: 'root',
     password: 'root',
     port: 8889,
-    database: 'bamazon' //--------change to intended database!!!---------
+    database: 'bamazonDB' //--------change to intended database!!!---------
 });
 
-
+// Running this application will first display all of the items available for sale.
+// Include the ids, names, and prices of products for sale.
 connection.connect(function(err) {
     if (err) throw err;
     console.log('you are connect as id ' + connection.threadId);
-})
+    showAll();
+});
+
+function showALL() {
+    connection.query('SELECT * FROM bamazonDB', function(err, res) {
+        if (err) throw err;
+        console.log(res);
+    });
+}
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+
+//The app should then prompt users with two messages.
+
+//* The first should ask them the ID of the product they would like to buy.
+//* The second message should ask how many units of the product they would like to buy.
+
+// function which prompts the user for what action they should take
+function start() {
+    inquirer
+        .prompt([{
+                name: "itemSelection",
+                type: "input",
+                message: "What is the item identification number of the product you would like to purchase today?"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?"
+            },
+
+        ])
+        // Once the customer has placed the order, your application should check if your
+        // store has enough of the product to meet the customer's request.If not, the app
+        // should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+        // However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+        // This means updating the SQL database to reflect the remaining quantity.
+        // Once the update goes through, show the customer the total cost of their purchase.
+}
--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE bamazon;
+CREATE DATABASE bamazonDB;
 USE bamazon;
 
 CREATE TABLE products(
@@ -11,31 +11,31 @@ CREATE TABLE products(
 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Frisbee', 'Outdoor Sports', '$8.00', 13);
+    VALUES (288, 'Frisbee', 'Outdoor Sports', '$8.00', 13);
 
     INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Golf Balls', 'Outdoor Sports', '$25.00', 10);
+    VALUES (837, 'Golf Balls', 'Outdoor Sports', '$25.00', 10);
 
     INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Sleeping Bag', 'Camping', '$45.00', 6);
+    VALUES (174, 'Sleeping Bag', 'Camping', '$45.00', 6);
 
     INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Cowboy Hat', 'Headwear', '$75.00', 4);
+    VALUES (395, 'Cowboy Hat', 'Headwear', '$75.00', 0);
 
     INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Flip Flops', 'Footwear', '$22.00', 15);
+    VALUES (853, 'Flip Flops', 'Footwear', '$22.00', 15);
 
     INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Calculator', 'Gadgets', '$5.00', 21);
+    VALUES (937, 'Calculator', 'Gadgets', '$5.00', 21);
 
     INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Dog Collar', 'Pet Items', '$8.00', 9);
+    VALUES (168, 'Dog Collar', 'Pet Items', '$8.00', 9);
 
     INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Cat Food', 'Pet Items', '$11.00', 17);
+    VALUES (729, 'Cat Food', 'Pet Items', '$11.00', 17);
 
     INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Dog Bed', 'Pet Items', '$36.00', 12);
+    VALUES (367, 'Dog Bed', 'Pet Items', '$36.00', 12);
 
     INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-    VALUES (001, 'Baseball Glove', 'Outdoor Sports', '$44.00', 12);
+    VALUES (929, 'Baseball Glove', 'Outdoor Sports', '$44.00', 12);
