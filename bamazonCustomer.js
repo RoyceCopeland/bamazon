d6d8f1f645214c3507ce8e3129ab93e2270e00a7
Royce Copeland <rcopeland3@Royce-Copelands-MacBook-Pro.local>
2017-08-18 08:13:54 -0400

    code complete. Time to complete comments and read.me

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,18 +8,18 @@ var connection = mySql.createConnection({
     user: 'root',
     password: 'root',
     port: 8889,
-    database: 'bamazonDB' //--------change to intended database!!!---------
+    database: 'bamazonDB' //-------- note: change to intended database ---------
 });
 
-// Running this application will first display all of the items available for sale.
-// Include the ids, names, and prices of products for sale.
+
 connection.connect(function(err) {
     if (err) throw err;
     //  console.log('you are connected as id ' + connection.threadId);
 
 });
 
-
+// Running this application will first display all of the items available for sale.
+// Include the ids, names, and prices of products for sale.
 function showAll() {
     connection.query('SELECT * FROM products', function(err, res) {
         if (err) throw err;
@@ -27,27 +27,19 @@ function showAll() {
         console.log('================= Here Are Our Items For Sale ! ==================');
         console.log('==================================================================');
         for (i = 0; i < res.length; i++) {
-            console.log('Product ID #:' + res[i].item_id + '   Product Name: ' + res[i].product_name + '   Price: ' + '$' + res[i].price + '  We have ' + res[i].stock_quantity + ' left!')
+            console.log('Product ID #:' + res[i].item_id + '   Product Name: ' + res[i].product_name + '   Price: ' + '$' + res[i].price);
         }
         console.log('==================================================================');
-        //placeOrder();
+
     })
 }
 showAll();
 
+// run the runStartShopping() function to prompt the user
 // delay the first inquiry until after the product list loads (utilizes the "delayed" module from npm)
 delayed.delay(function() { runStartShopping(); }, 1000)
 
-// run the start function after the connection is made to prompt the user
-// start();
-
-
-//The app should then prompt users with two messages.
-
-//* The first should ask them the ID of the product they would like to buy.
-//* The second message should ask how many units of the product they would like to buy.
-
-
+// Welcome message to user
 function runStartShopping() {
     inquirer
         .prompt({
@@ -75,6 +67,10 @@ function runStartShopping() {
             }
         });
 }
+// The app should then prompt users with two messages.
+
+// The first should ask them the ID of the product they would like to buy (chosen from the displayed list).
+// The second message should ask how many units of the product they would like to buy.
 
 function itemSearch() {
     inquirer
@@ -87,54 +83,56 @@ function itemSearch() {
             {
                 name: "quantity",
                 type: "input",
-                message: "... and how many of these would you like?"
+                message: "... and how many would you like?"
             }
-
         ])
-        .then(function(answer) {
-            var query = "SELECT * FROM products WHERE ?";
-            connection.query(query, { item_id: answer.item }, function(err, res) {
-                // for (var i = 0; i < res.length; i++) {
-                console.log("You chose " + (answer.quantity) + " of the: " + (res[0].product_name));
-                if (answer.quantity < res[0].stock_quantity) {
-                    console.log("We have that in stock!");
-                    var newStockQuantity = (res[0].stock_quantity - answer.quantity);
-                    // update the database here  (update query w sql) 
 
-                    var sql = "UPDATE products SET stock_quantity = " + newStockQuantity + " WHERE item_id = " + answer.item;
-                    connection.query(sql, function(err, result) {
-                        if (err) throw err;
-                        console.log(result.affectedRows + " record(s) updated");
-                        console.log(result);
-                    });
+    // Once the customer has placed the order
 
-                } else {
-                    console.log("Sorry, we don't have that many :(");
-                }
+    // use the shopper's responses to the two inquiries (answer.item and answer.quantity)
+    .then(function(answer) {
+        var query = "SELECT * FROM products WHERE ?";
+        connection.query(query, { item_id: answer.item }, function(err, res) {
+            console.log(res);
+            // returns this ... a single object with all data related to item_id 853...
+            // [ RowDataPacket {
+            //       item_id: 853,
+            //       product_name: 'Flip Flops',
+            //       department_name: 'Footwear',
+            //       price: 22,
+            //       stock_quantity: 15 } ]
+            // therefore, we use use (res[0].product_name) to pull the name of the product from the 
+            // first ->[0] object in the res(ponse).
+
+            console.log("You chose " + (answer.quantity) + " of the " + (res[0].product_name));
+
+            // check if your store has enough of the product to meet the customer's request.
+            if (answer.quantity < res[0].stock_quantity) {
+                console.log("We have that in stock!");
+                var salesTotal = answer.quantity * res[0].price;
+                console.log("Your total is $" + salesTotal + ".00  Thank you for choosing Bamazon :)");
+                var newStockQuantity = (res[0].stock_quantity - answer.quantity);
 
 
+                // update the database inventory (stock_quantity) 
+                // You can update existing records in a table by using the Node.js MySql "UPDATE" statement:
 
-            })
-
-
-
-
-        });
+                var sql = "UPDATE products SET stock_quantity = " + newStockQuantity + " WHERE item_id = " + answer.item;
+                connection.query(sql, function(err, result) {
+                    if (err) throw err;
+                    console.log(result.affectedRows + " record(s) updated");
+                    console.log(result);
+                });
+                // If not, the app should log a phrase like `Insufficient quantity!`,
+                // and then prevent the order from going through.
+            } else {
+                console.log("Sorry, we don't have that many :( Please check back again tomorrow!");
+            }
+        })
+    });
 }
 
 
-
-
-
-
-
-//* The second message should ask how many units of the product they would like to buy.
-
-
-
-// Once the customer has placed the order, your application should check if your
-// store has enough of the product to meet the customer's request.If not, the app
-// should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
 // However, if your store _does_ have enough of the product, you should fulfill the customer's order.
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
